8ba4ec423dc1e169a30cb9542dc41296de754b25
Cesar "Tito" Lopez <cesar.lopez.rodriguez@gmail.com>
2019-06-07 22:13:22 -0400

    I have had some problems with the layout and objects handling...I uploading my work as is but I will accomplish the task during the weekend, sorry about that

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 //  trivia - game activity - How is your knowledge of the world 
 //  ****
 
@@ -6,70 +5,116 @@
 
 
 
-var countStartNumber = 30;
 
-var clockruning = false;
+// var questions = ["Which is the capital of Finland?",
+//     "which team won the Soccer Champions League?",
+//     "Who runs SPACE X, company specialized in rockets and spacecraft?",
+//     "Who is the current president of France?"];
 
-var questions = ["Which is the capital of Finland?",
-    "which team won the Soccer Champions League?",
-    "Who runs SPACE X, company specialized in rockets and spacecraft?",
-    "Who is the current president of France?"];
+//  var alternatives1 = ["Stockolm", "Geneve", "Helsinsky", "Melbourne"];
+//  var alternatives2 = ["Real Madrid", "Liverpool", "Ajax", "Orlando Lions"];
+//  var alternatives3 = ["Richard Banson", "Warren Buffet", "Darth Vader", "Elon Musk"];
+//  var alternatives4 = ["De gaulle", "Asterix", "Lacoste", "Macron"]//
 
-var alternatives1 = ["Stockolm", "Geneve", "Helsinsky", "Melbourne"];
-var alternatives2 = ["Real Madrid", "Liverpool", "Ajax", "Orlando Lions"];
-var alternatives3 = ["Richard Banson", "Warren Buffet", "Darth Vader", "Elon Musk"];
-var alternatives4 = ["De gaulle", "Asterix", "Lacoste", "Macron"]
 
-correctAnswersIndex[2 , 1 , 3 , 3];
-// set interval timer
-var timer = 0;
+
+
+
+// var questions = ["Which is the capital of Finland?","Stockolm", "Geneve", "Helsinsky", "Melbourne",
+//                 "which team won the Soccer Champions League?", "Real Madrid", "Liverpool", "Ajax", "Orlando Lions",
+//                 "Who runs SPACE X, company specialized in rockets and spacecraft?","Richard Banson", "Warren Buffet", "Darth Vader", "Elon Musk",
+//                 "Who is the current president of France?", "De gaulle", "Asterix", "Lacoste", "Macron"];
+
+
+//  var correctAnswersIndex = [2 , 1 , 3 , 3];
+
+
+ var questionsMaster = [
+     { question:" Which is the capital of Finland?",
+     answers: [" a)Stockolm ", " b)Geneve ", " c)Helsinsky ", " d)Melbourne "],
+     correct: "c)Helsinsky"
+    },
+     { question:"pregunta",
+     answers: ["Answer1","answer2","answer3","answer4"],
+     correct: "Answer1"
+    },
+     { question:"pregunta",
+     answers: ["Answer1","answer2","answer3","answer4"],
+     correct: "Answer1"
+    },
+     { question:"pregunta",
+     answers: ["Answer1","answer2","answer3","answer4"],
+     correct: "Answer1"
+    }
+ ]
+
+ // set interval timer
+var timer = 30;
 var intervalId;
+var questionNumber = 1;
 
 // Count will keep track of the index of the currently question  
 var counter = 0;
 
 //start  button
-$(document).on("click", "#start", loadQuestion() {
-    $("#sub-wrapper").prepend("<h2>  ")
-
-
-function start()
-if (clockruning != ) {
-
-clockruning = true;    
-//// creare\ click events 
-/
-}
-
-
-function countdown () {
-        //decrement counter 
-        // use jquery to put dynamicaly put logic
-        counter--;
-        $("#sub-wrapper").html ("<h2>" + counter +"</h2>");
-
-        if (counter === 0 ) {
-
-            timeUp();
-        }
-
-}
+// $(document).ready(function();
+// {
+    counter = 0;
+    $("#start").click(function(){
+ 
+     $("#Quiz-area").append("<h2>Question </h2>");
+                    
+        // var card = $("#Quiz-area");
+        
+       loadQuestion();
+})
 
 function loadQuestion() {
 
     //set the timer 
     //timer = set interval (game .countdown ,1000)
-    timer = setInterval(game.countdown,1000);
-    // dynamically add question into card variable  card.ht,ml ("<h2>" +"</h2>'")
-    var card = $("#quiz-area");
-}
+    // intervalId = setInterval(countdown,1000);
+    // dynamically add question into card variable  card.html ("<h2>" +"</h2>'")
+    
 
+    var card = $("#Quiz-area")
+    card.append(questionsMaster[counter].question[counter]);
+        card.append(questionsMaster[counter].answers[counter + 1]);
+        card.append(questionsMaster[counter].answers[counter + 2]);
+        card.append(questionsMaster[counter].answers[counter + 3]);
+        card.append(questionsMaster[counter].answers[counter + 4]);
+    }
+
+    
 function nextQuestion() {
     //set a counteer 
     //game counter
     //use jquery to change the text of the game counter 
     //increment the currenteQuestion by one 
     //call the loadquestioin function
+    
+        //set the timer 
+        //timer = set interval (game .countdown ,1000)
+        intervalId = setInterval(countdown,1000);
+        // dynamically add question into card variable  card.ht,ml ("<h2>" +"</h2>'")
+        var card = $("#quiz-area");
+        topValue = index  + 4;
+        card.append(question[index]);
+         for (var index = 0; 0 < topValue; index++) {
+               
+            card.append(question[index]);
+        }
+        
+        function countdown () {
+            //decrement counter 
+            // use jquery to put dynamicaly put logic
+            counter--;
+            $("#sub-wrapper").html ("<h2>" + counter +"</h2>");
+            if (counter === 0 ) {
+                timeUp();
+            }
+        }
+    
 }
 
 function timeUp() {
@@ -79,18 +124,23 @@ function timeUp() {
    
    }
 
-function answeredIncorrectly() {
-
-
-}
-
-
-
+function correctAnswer() {
 
 
 
 }
+
+function incorrectAnswer() {
+
+
 }
+function stop(){
+
+
+}
+// clear internal varibles 
+// intervalId 
+
 
 
 
@@ -102,7 +152,7 @@ var counter = 0;
 
 
 //    var yourScore = 0;
-$("#corrects-text").text(corrects)
+$("#corrects-text").text(corrects);
 $("incorrects-text").text(incorrects);
 $("#score-text").text(counter);
 
@@ -118,53 +168,48 @@ $("#score-text").text(counter);
 // new iteraction
 
 
-for (let index = 0; index < questions.length; index++) {
+// for (let index = 0; index < questions.length; index++) {
 
-    // load questions
+//     // load questions
 
-    // Lastly, each question will get added to the page.
-    $("#sub-wrapper").prepend(questions[index]);
+//     // Lastly, each question will get added to the page.
+//     $("#sub-wrapper").prepend(questions[index]);
 
-    // This time, our click event applies to every single crystal on the page. Not just one.
-}
-$(".document").on("click", ".answer-button", function (e) {
-    game.clicked(e);
-});
-
-
-var crystalValue = ($(this).attr("data-crystalvalue"));
-crystalValue = parseInt(crystalValue);
-
-// We then add the crystalValue to the user's "counter" which is a global variable.
-// Every click, from every crystal adds to the global counter.
-counter += crystalValue;
-
-// console.log(counter)
-
-if (counter === targetNumber) {
-    wins++;
-    $("#wins-text").text(wins);
-    counter = 0;
-    $("#score-text").text(counter)
-    getRandom();
-    // winsText.textContent = "wins: " + wins;
-    // var winsText = document.getElementById("wins-text");
-} else if (counter > targetNumber) {
-    losses++;
-    $("#losses-text").text(losses);
-    counter = 0;
-    $("#score-text").text(counter)
-    getRandom();
-    // var lossesText = document.getElementById("losses-text");
-    // lossesText.textContent = "losses: " + losses;
-
-} else $("#score-text").text(counter);
+//     // This time, our click event applies to every single crystal on the page. Not just one.
+// }
+// $(".document").on("click", ".answer-button", function (e) {
+//     game.clicked(e);
+// });
 
 
-});
+// var crystalValue = ($(this).attr("data-crystalvalue"));
+// crystalValue = parseInt(crystalValue);
 
-getRandom();
-</script >
-</body >
+// // We then add the crystalValue to the user's "counter" which is a global variable.
+// // Every click, from every crystal adds to the global counter.
+// counter += crystalValue;
 
-</html >
+// // console.log(counter)
+
+// if (counter === targetNumber) {
+//     wins++;
+//     $("#wins-text").text(wins);
+//     counter = 0;
+//     $("#score-text").text(counter)
+//     getRandom();
+//     // winsText.textContent = "wins: " + wins;
+//     // var winsText = document.getElementById("wins-text");
+// } else if (counter > targetNumber) {
+//     losses++;
+//     $("#losses-text").text(losses);
+//     counter = 0;
+//     $("#score-text").text(counter)
+//     getRandom();
+//     // var lossesText = document.getElementById("losses-text");
+//     // lossesText.textContent = "losses: " + losses;
+
+// } else $("#score-text").text(counter);
+
+
+// });
+
